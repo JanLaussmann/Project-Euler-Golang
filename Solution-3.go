580c8483ef6c302e3c3895dbc6219e81196180d2
JanLaussmann <jan.laussmann@gmail.com>
2013-06-07 22:59:07 +0200

    Solution to problem 3

---
+++ Solution-3.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a int64 = 600851475143
+
+	for b := sqrt(a); b >= 1; b-- {
+		if a % b == 0 && isPrime(b) {
+			fmt.Println(b)
+			break
+		}
+	}
+}
+
+func isPrime(a int64) bool {
+	if a <= 1 {
+		return false
+	}
+	
+	for b := sqrt(a); b >= 1; b-- {
+		if b == 1 {
+			return true
+		}
+		if a % b == 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func sqrt(a int64) int64 {
+	return int64(math.Sqrt(float64(a)))
+}
